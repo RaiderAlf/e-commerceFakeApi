30cec270c71bab694093bf755165f5a3d8d7aa8d
Kevin Alfonzo <kalfonzoespinett@gmail.com>
2023-11-19 12:23:09 -0400

    UI/UX

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -41,7 +41,8 @@ const Card = (product) => {
                                 leaveFrom="opacity-100 scale-100"
                                 leaveTo="opacity-0 scale-95"
                             >
-                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                <Dialog.Panel className="w-full flex flex-col justify-center items-center max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                    <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4' onClick={closeModal} >X</button>
                                     <Dialog.Title
                                         as="h3"
                                         className="text-lg text-center font-medium leading-6 text-gray-900"
@@ -59,7 +60,7 @@ const Card = (product) => {
                                         <p className="mt-1 text-center font-medium text-gray-900">{product.price} $</p>
                                         <p className="mt-1 text-center text-sm text-gray-900">{product.description} $</p>
                                         <br />
-                                        <div className="flex items-center">
+                                        <div className="flex justify-center items-center">
                                             <svg className="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                                                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                                             </svg>
