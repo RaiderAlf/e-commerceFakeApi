9870e230fa950d36e1bf49630e7c57a6c93d75bb
Kevin Alfonzo <kalfonzoespinett@gmail.com>
2023-12-12 17:01:12 -0400

    fix style hero

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
 
 
 
-                    <section className="bg-white [box-shadow:_0_0_10px_rgb(0_0_0_/_40%)] rounded-lg">
+                    <section className="bg-white [box-shadow:_0_0_10px_rgb(0_0_0_/_40%)] my-4 rounded-lg">
                         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                             <div className="mr-auto place-self-center lg:col-span-7 p-2">
                                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Lorem ipsum dolor sit amet.</h1>
