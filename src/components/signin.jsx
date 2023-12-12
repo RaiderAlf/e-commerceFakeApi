b2db89740a8339c35dc2298e8df39027e8b5e2a4
Kevin Alfonzo <kalfonzoespinett@gmail.com>
2023-12-12 15:03:35 -0400

    signin form bug fixes

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,7 +1,8 @@
 //DEPENDENCIES
 import axios from "axios";
 //HOOKS
-import { useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from "react-router-dom";
 //ACTIONS
@@ -9,11 +10,25 @@ import { addUser, removeUser } from "../redux/actions";
 //COMPONENTS
 import NavBarLogin from "./navbarLogin"
 
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL
+})
+
+
 const Signin = () => {
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
+    let [alertOpen, setAlertOpen] = useState(false)
+
+    function closeAlert() {
+        setAlertOpen(false)
+    }
+
+    function openAlert() {
+        setAlertOpen(true)
+    }
 
 
     const handleSubmit = (e) => {
@@ -22,10 +37,27 @@ const Signin = () => {
 
         dispatch(removeUser())
 
-        axios.post('https://microservices-api.onrender.com/signin', inputForm)
-            .then(data => dispatch(addUser(data.data)), navigate('/'))
+        axiosInstance.post('/signin', inputForm)
+            .then(data => {
+                if (data.data) {
+
+                    dispatch(addUser(data.data))
+                    navigate('/')
+
+                }
+            })
+            .catch(error => {
+                setErrState({
+                    message: error.response.data.message
+                })
+                openAlert()
+            })
 
     };
+
+    const [errState, setErrState] = useState({
+        message: ""
+    })
 
     const [inputForm, setInputForm] = useState({
         email: "",
@@ -41,6 +73,59 @@ const Signin = () => {
 
     return (
         <>
+
+            <Transition appear show={alertOpen} as={Fragment}>
+                <Dialog as="div" className="fixed inset-0" onClose={closeAlert}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black/50" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 ">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel onClick={closeAlert} className="w-fit flex flex-col items-center justify-center max-w-md text-blue-900 transform overflow-hidden rounded-2xl bg-slate-100 p-6 text-left align-middle shadow-xl transition-all" role="alert">
+                                    <button
+                                        type="button"
+                                        className="flex flex-col items-center justify-center gap-1 text-slate-800 hover:text-black-500"
+                                        onClick={closeAlert}
+                                    >
+                                        <span className="absolute -inset-0.5" />
+                                        <span className="sr-only">Close panel</span>
+                                        <svg className="flex-shrink-0 inline w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 20 20">
+                                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                                        </svg>
+                                    </button>
+                                    <div className='flex items-center justify-center'>
+                                        <span className="sr-only text-red">Info</span>
+                                        <div className='flex flex-col items-center justify-center gap-1'>
+                                            <span className="font-medium text-red-700">Error</span>
+                                            <span className="font-bold text-red-900">{errState.message}</span>
+                                        </div>
+                                    </div>
+
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+
             <NavBarLogin />
             <form className="max-w-sm mx-auto my-40">
                 <h1 className="text-slate-500 text-center text-lg font-medium my-8 [text-shadow:_0_5px_5px_rgb(0_0_0_/_50%)]">
