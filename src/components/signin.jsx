787c224f21ce35e2595c4f113851330d709be208
RaiderAlf <kalfonzoespinett@gmail.com>
2023-12-17 20:40:35 -0400

    handler btn

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -25,6 +25,12 @@ const Signin = () => {
 
     const [loader, setLoader] = useState(false)
 
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    };
+
     function closeAlert() {
         setAlertOpen(false)
     }
@@ -45,7 +51,9 @@ const Signin = () => {
         axiosInstance.post('/signin', inputForm)
             .then(data => {
                 if (data.data) {
-                    Cookies.set('user', JSON.stringify(data.data))
+                    if (isChecked) {
+                        Cookies.set('user', JSON.stringify(data.data))
+                    }
                     setLoader(false)
                     dispatch(addUser(data.data))
                     navigate('/')
@@ -230,12 +238,12 @@ const Signin = () => {
                     </div>
                     <label htmlFor="pass" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-slate-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
-                {/* <div className="flex items-start mb-5 my-4">
+                <div className="flex items-start mb-5 my-4">
                     <div className="flex items-center h-5">
-                        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-slate-100 rounded bg-gray-50 focus:ring-3 focus:ring-slate-300" required />
+                        <input id="remember" type="checkbox" checked={isChecked} onChange={handleCheckboxChange} className="w-4 h-4 border border-slate-100 rounded bg-gray-50 focus:ring-3 focus:ring-slate-300" />
                     </div>
                     <label htmlFor="remember" className="ms-2 text-sm font-medium text-slate-400">Remember me</label>
-                </div> */}
+                </div>
                 {
                     inputForm.email !== "" && inputForm.password !== "" ? (
                         <button onClick={handleSubmit} type="submit" className="text-white bg-slate-700 hover:[box-shadow:inset_0_0_5px_rgb(255_255_255_/_50%)] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 my-4 text-center">Submit</button>
