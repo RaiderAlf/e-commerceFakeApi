09014896f0bea6e6626b0799c750bd520222bb50
RaiderAlf <kalfonzoespinett@gmail.com>
2024-01-23 16:26:48 -0400

    fix footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
 
         <footer className="bg-slate-100 w-full">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center">© 2023 <a href="#" className="hover:underline">Kevin Alfonzo DEV®  </a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center">© 2023 <a href="#" className="hover:underline">EmpresaDEV®  </a>. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
                     <li>
